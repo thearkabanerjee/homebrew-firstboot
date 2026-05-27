f343deab6ea1338bf527207522a5ee58bde412b6
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-27 13:37:10 +0530

    Release v1.0.1

--- Formula/firstboot.rb
+++ Formula/firstboot.rb
@@ -2,7 +2,7 @@ class Firstboot < Formula
   desc "Bootstrap a macOS developer environment"
   homepage "https://github.com/thearkabanerjee/firstboot"
   url "https://github.com/thearkabanerjee/firstboot/releases/download/v1.0.0/firstboot-v1.0.0.tar.gz"
-  sha256 "d82013fb586860dab15a6cc8e32de84904ee3908c2be37ffc43d5ffb34aa7579"
+  sha256 "7be95fdf21939ebf54866cc408cce77d8b813920a6454c4b1184f4462665446e"
   version "1.0.0"
 
   def install
