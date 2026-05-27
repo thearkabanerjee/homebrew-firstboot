bdf9e71a2037f1b42f7f925dc7a6574bda3ce06c
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-27 13:37:32 +0530

    Release v1.0.1

--- Formula/firstboot.rb
+++ Formula/firstboot.rb
@@ -3,7 +3,7 @@ class Firstboot < Formula
   homepage "https://github.com/thearkabanerjee/firstboot"
   url "https://github.com/thearkabanerjee/firstboot/releases/download/v1.0.0/firstboot-v1.0.0.tar.gz"
   sha256 "7be95fdf21939ebf54866cc408cce77d8b813920a6454c4b1184f4462665446e"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "firstboot"
